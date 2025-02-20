10b394b5c302b48a8d38216f1f33dd9e8abe72ea
chryswds <dybas.chrys@gmail.com>
2025-02-20 15:00:31 +0000

    feat: creates scheduling service

--- src/main/java/com/chrystian/notificatio_scheduling_api/business/SchedulingService.java
+++ src/main/java/com/chrystian/notificatio_scheduling_api/business/SchedulingService.java
@@ -1,4 +1,17 @@
-package com.chrystian.notificatio_scheduling_api.business.mapper;
+package com.chrystian.notificatio_scheduling_api.business;
 
+
+import com.chrystian.notificatio_scheduling_api.infrastructure.repositories.SchedulingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class SchedulingService {
+
+    private final SchedulingRepository repository;
+
+
+
+
 }
