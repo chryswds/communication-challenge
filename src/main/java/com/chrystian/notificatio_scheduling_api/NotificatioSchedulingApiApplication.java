547a7936d2f65dc030df73fa84d76785f865bfb9
chryswds <dybas.chrys@gmail.com>
2025-02-20 14:19:48 +0000

    first commit

---
+++ src/main/java/com/chrystian/notificatio_scheduling_api/NotificatioSchedulingApiApplication.java
@@ -0,0 +1,13 @@
+package com.chrystian.notificatio_scheduling_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificatioSchedulingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificatioSchedulingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrystian/notificatio_scheduling_api/NotificatioSchedulingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrystian.notificatio_scheduling_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificatioSchedulingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
