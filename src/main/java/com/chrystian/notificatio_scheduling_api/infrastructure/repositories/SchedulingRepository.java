5c900609eaef3cefedaacf98c241e2ee56900fc0
chryswds <dybas.chrys@gmail.com>
2025-02-20 15:01:05 +0000

    feat: creates scheduling repository that extends from JPA

--- src/main/java/com/chrystian/notificatio_scheduling_api/infrastructure/repositories/SchedulingRepository.java
+++ src/main/java/com/chrystian/notificatio_scheduling_api/infrastructure/repositories/SchedulingRepository.java
@@ -1,4 +1,7 @@
 package com.chrystian.notificatio_scheduling_api.infrastructure.repositories;
 
-public interface SchedulingRepository {
+import com.chrystian.notificatio_scheduling_api.infrastructure.entities.Scheduling;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchedulingRepository extends JpaRepository<Scheduling, Long> {
 }
